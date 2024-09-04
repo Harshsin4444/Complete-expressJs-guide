f79c63aa5953877b4da30429c91a13db87440d5d
Harshsin4444 <singhalharsh4444@gmail.com>
2024-09-04 19:27:50 +0530

    second commit

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">SEND</button></form>');
+
 })
 
-app.use((req,res,next)=>{
-    console.log('in the another middleware');
-    res.send('<h1>Hello from ExpressJs</h1>');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 })
+
+app.use('/',(req,res,next)=>{
+    console.log('in the middleware');
+    res.send('<h1>HELLO WORLD</h1>');
+})
+
 
 app.listen(3000);
